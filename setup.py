cbf29a7b302133879a01ebcd1c5b3b7116508a8b
Alexander Andryukov <aandryukov@more.tv>
2022-05-06 20:12:42 +0500

    add_one

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-package",
+    version="0.0.1",
+    author="Alexander Andryukov",
+    author_email="andryukov@gmail.com",
+    description="First package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/someth",
+    project_urls={"Bug Tracker": "https://gitlab.com/someth/bug_tracker"},
+    classifiers=["Programming Language :: Python :: 3", ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8"
+)
